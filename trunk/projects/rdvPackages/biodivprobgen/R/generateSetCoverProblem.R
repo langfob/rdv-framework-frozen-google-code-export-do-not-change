0072839757a2261715994d9df2f2c39cda267147
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2015-04-10 01:39:59 +0000

    biodivprobgen: Added code to verify that the completed edge_list has been built correctly, i.e., it does not violate any assumptions about endpoints of the edges being dependent or independent, etc.

--- trunk/projects/rdvPackages/biodivprobgen/R/generateSetCoverProblem.R
+++ trunk/projects/rdvPackages/biodivprobgen/R/generateSetCoverProblem.R
@@ -150,7 +150,10 @@
     #  debugging level: 0 means don't output debugging write statements.
     #  Having this as an integer instead of binary so that I can have 
     #  multiple levels of detail if I want to.
-DEBUG_LEVEL = 1
+DEBUG_LEVEL = 0
+
+    #  Turn all R warnings into errors.
+option (warn=2)
 
 #===============================================================================
 
