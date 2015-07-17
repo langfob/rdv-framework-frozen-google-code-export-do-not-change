b3965f3ec194f222a901490ef056e81b594f797f
heini.kujala <heini.kujala@gmail.com>
2015-07-17 02:40:01 +0000

    spp-inf: Within distribution richness and co-occurrence calculations changed to include continuous distribution data (prob or likelihoods). Code for plots transferred to separate script.

--- projects/spp-inf/Creating species-site matrix and creating species info table.R
+++ projects/spp-inf/Creating species-site matrix and creating species info table.R
@@ -1,3 +1,6 @@
+
+rm(list = ls())
+
 library(raster)
 library(sp)
 library(picante)
@@ -20,8 +23,9 @@ for(i in seq_along(sp)) {
   sp_site_m[, i] <- na.omit(as.vector(raster(sprintf(sp_dist_path, i))))
 }
 
-sp_site_m[sp_site_m[] > 0.5] <- 1 #NB! what threshold do we need to use?
-sp_site_m[sp_site_m[] < 1] <- 0
+# If distributions need to be thresholded to presence-absences
+#sp_site_m[sp_site_m[] > 0.5] <- 1 #NB! what threshold do we need to use?
+#sp_site_m[sp_site_m[] < 1] <- 0
 
 
 
@@ -32,9 +36,9 @@ sp_info <- data.frame(sp=names, dist_sum=NA, dist_richness=NA, mean_jaccard_simi
 # distribution size (from original probability values)
 sp_info$dist_sum <- read.table('Output/out_all.features_info.txt', skip=1, header=T, sep='')[,2]
 
-# average sp richness within distribution
+# average weighted sp richness within distribution (suitable for both continuous and binary data)
 for (i in 1:length(names)){
-  sp_info$dist_richness[i] <- mean(apply(sp_site_m[sp_site_m[,i]==1,], 1, sum)-1)
+  sp_info$dist_richness[i] <- weighted.mean(apply(sp_site_m[,-i], 1, sum), sp_site_m[,i])
 }
 
 # calculate co-occurrence using 'picante' package
@@ -46,28 +50,3 @@ for (i in 1:length(names)){
 }
 
 save(sp_info, file='C:/Users/hkujala/work/Species influence/src/sp_info.r')
-
-
-
-# plots
-load(file='C:/Users/hkujala/work/Species influence/src/sp_info.r')
-load(file='C:/Users/hkujala/work/Species influence/src/sp_influence.r') # NB! object called 'results_all'
-
-# absolute differene in ranks
-png("Absolute difference in ranks.png", height=12, width=15, units="cm", res=300, bg="transparent", pointsize=11)
-par(mfrow=c(2,2), mar=c(4,2,1,1), oma=c(0,3,0,0))
-plot(sp_info$dist_sum, results_all$Rank_diff, ylab='', xlab='Distribution sum', pch=19)
-plot(sp_info$dist_richness, results_all$Rank_diff, ylab='', xlab='Average sp richness within distribution', pch=19)
-plot(sp_info$mean_jaccard_similarity, results_all$Rank_diff, ylab='', xlab='Mean Jaccard similarity', pch=19)
-mtext('Absolute difference in ranks', 2, outer=T, line=1)
-dev.off()
-
-
-# Sum of AUCs
-png("Sum of AUCs.png", height=12, width=15, units="cm", res=300, bg="transparent", pointsize=11)
-par(mfrow=c(2,2), mar=c(4,2,1,1), oma=c(0,3,0,0))
-plot(sp_info$dist_sum, results_all$AUC_sum, ylab='', xlab='Distribution sum', pch=19)
-plot(sp_info$dist_richness, results_all$AUC_sum, ylab='', xlab='Average sp richness within distribution', pch=19)
-plot(sp_info$mean_jaccard_similarity, results_all$AUC_sum, ylab='', xlab='Mean Jaccard similarity', pch=19)
-mtext('Sum of AUCs', 2, outer=T, line=1)
-dev.off()
